3711d430cc2f44aadf61e9ad9f3de19a9b087a13
sanjayspear <sanjayspear@gmail.com>
2024-09-02 15:41:49 +0530

    Locate Frames Approcah 2

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -5,11 +5,17 @@ test('Interact with Frames', async ({page}) => {
     // Counts no. of frames
     const allFrames = page.frames();
     console.log("No. frames: "+allFrames.length);
-    //Locate your frame
+    //Locate your frame (Approach 1)
     const myFrame = page.frame("firstFr");
     await myFrame?.fill("//input[@name='fname']", "Sanjay");
     await myFrame?.fill("//input[@name='lname']", "Verma");
     await page.waitForTimeout(3000);
     //expect(await myFrame?.locator('p.has-text-info').textContent()).toContain("You have entered");
     expect(await myFrame?.locator('p.has-text-info').textContent()).toBe("You have entered Sanjay Verma");
+
+    //Locate your frame (Approach 2)
+    const frame = page.frameLocator("#firstFr");
+    await frame.locator("//input[@name='fname']").fill("Ankit");
+    await frame.locator("//input[@name='lname']").fill("Sharma");
+
 });
