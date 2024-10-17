f95a6509c317a070eb8830933c521e5853ebcbe3
sanjayspear <sanjayspear@gmail.com>
2024-10-17 13:09:33 +0530

    modified:   pages/homePage.ts

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export default class HomePage{
     constructor(public page: Page){
@@ -13,5 +13,10 @@ export default class HomePage{
         await this.page.locator("'Mega Menu'").hover(); //locator type is linked text.
         //await this.page.locator("'HTC'").click();
         await this.page.locator(`'${productName}'`).click();
+    }
+
+    async logoColorCheck(): Promise<Locator>{
+        const element = this.page.locator("(//*[contains(@class, 'entry-design') and contains(@class, 'design-image') and contains(@class, 'flex-grow-0') and contains(@class, 'flex-shrink-0')])[1]");
+        return element;
     }
 }
