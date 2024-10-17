ccbb185812ed9b9ffdb7e7b863de138bf0cce76e
sanjayspear <sanjayspear@gmail.com>
2024-10-17 13:08:57 +0530

    modified:   pages/addProductToCart.ts

--- pages/addProductToCart.ts
+++ pages/addProductToCart.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export default class AddProductToCart{
     constructor(public page: Page){
@@ -14,6 +14,11 @@ export default class AddProductToCart{
         const toast =  this.page.locator("//a[.='View Cart ']");
         await toast.waitFor({state: 'visible'});
         return toast;
+    }
+
+    async logoColorCheck(): Promise<Locator>{
+        const element = this.page.locator("(//*[contains(@class, 'entry-design') and contains(@class, 'design-image') and contains(@class, 'flex-grow-0') and contains(@class, 'flex-shrink-0')])[1]");
+        return element;
     }
 
 }
